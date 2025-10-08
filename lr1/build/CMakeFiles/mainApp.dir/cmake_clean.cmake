9bda59eb8149ee69199bdb5468b623a4bf0d725b
mr_ilyablogger <ilya5111082467@gmail.com>
2025-10-08 15:00:11 +0300

    lr2 creation

---
+++ lr1/build/CMakeFiles/mainApp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mainApp.dir/main.cpp.o"
+  "CMakeFiles/mainApp.dir/main.cpp.o.d"
+  "CMakeFiles/mainApp.dir/src/oop_lr1.cpp.o"
+  "CMakeFiles/mainApp.dir/src/oop_lr1.cpp.o.d"
+  "mainApp"
+  "mainApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mainApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
